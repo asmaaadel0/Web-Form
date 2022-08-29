5cfb72273f5325db0fd6378f628da0c869d4d0d8
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-08-29 22:49:54 +0200

    add signup form component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<signup-form></signup-form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignupForm from './components/SignupForm.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SignupForm
+    
   }
 }
 </script>
@@ -22,5 +22,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body{
+  margin: 0;
+  background: #eee;
 }
 </style>
---
+++ src/components/SignupForm.vue
@@ -0,0 +1,55 @@
+<template>
+    <form>
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" required v-model="email">
+
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password" required v-model="password">
+
+        <label for="role">Role:</label>
+        <input type="email" name="role" id="role" required>
+
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+        }
+    }
+}
+</script>
+
+<style>
+form {
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+}
+
+label {
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+}
+
+input {
+    display: block;
+    padding: 10px 6px;
+    box-sizing: border-box;
+    width: 100%;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+}
+</style>
