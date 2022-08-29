df26991adc29d66fc5d393fce45c06a91c28424a
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-08-29 23:23:39 +0200

    add skills input

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -11,6 +11,11 @@
             <option value="developer">Web Developer</option>
             <option value="designer">Web Designer</option>
         </select>
+
+        <label for="skills">Skills:</label>
+        <input type="text" name="skills" id="skills" v-model="tempSkill" required @keyup="addSkill">
+        <div v-for="skill in skills" :key="skill" class="pill">{{  skill  }}</div>
+
         <div class="terms">
             <input type="checkbox" name="terms" id="terms" required v-model="term">
             <label for="terms">Accept terms and conditions</label>
@@ -25,7 +30,23 @@ export default {
             email: '',
             password: '',
             role: 'developer',
-            term:false,
+            term: false,
+            tempSkill: '',
+            skills: [],
+        }
+    },
+    methods: {
+        addSkill(e) {
+            if (e.key === ',' && this.tempSkill) {
+                if (this.skills.find(skill => skill === this.tempSkill.slice(0, -1))) {
+                    alert("You add this skill before!");
+                    this.tempSkill = "";
+                    return;
+                }
+                this.skills.push(this.tempSkill.slice(0, -1));
+                this.tempSkill = "";
+            }
+
         }
     }
 }
@@ -61,7 +82,8 @@ select {
     border-bottom: 1px solid #ddd;
     color: #555;
 }
-input[type="checkbox"]{
+
+input[type="checkbox"] {
     display: inline-block;
     width: 16px;
     margin: 0 10px 0 0;
